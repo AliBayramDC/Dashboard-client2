29b95c7dc4abf8021705ba6afed0192b33e58364
AliBayramDC <121183289+AliBayramDC@users.noreply.github.com>
2024-10-28 18:28:54 +0400

    Update Sidebar.jsx

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -96,20 +96,7 @@ const Sidebar = () => {
               setSelected={setSelected}
             />
 
-            <Typography
-              variant="h6"
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
-              Analyses
-            </Typography>
-            <Item
-              title="ABC"
-              to="/abc"
-              icon={<AbcIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
+            
 
             <Typography
               variant="h6"
